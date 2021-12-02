07125af29b2b9883d566eb1905d79724fe569dfa
Adolfo Diego <yakshim0@gmail.com>
2021-12-02 10:37:55 -0600

    creaciopn de forms y arreglo de erros en urls y views

--- curso/forms.py
+++ curso/forms.py
@@ -10,5 +10,5 @@ class MateriaForm(forms.ModelForm):
     def __init__ (self, *args, **kwargs):
         super(MateriaForm, self).__init__(*args, **kwargs)
         self.fields["alumnos"].widget = forms.widgets.CheckboxSelectMultiple()
-        self.fields["alumnos"].help_text = "Ingrese los alumnos del curso"
+        self.fields["alumnos"].help_text = "Ingrese los Alumnos asignados al curso"
         self.fields["alumnos"].queryset = Alumno.objects.all()
